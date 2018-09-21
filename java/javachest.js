17f15cf2c3851a951959b4eb972db637f2f9d4f3
kavi678 <kavipatel111.kp@gmail.com>
2018-09-21 19:34:37 +0930

    excersise details added for chest

---
+++ java/javachest.js
@@ -0,0 +1,32 @@
+function mychest1() {
+    var x = document.getElementById("chest1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function mychest2() {
+    var x = document.getElementById("chest2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function mychest3() {
+    var x = document.getElementById("chest3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function mychest4() {
+    var x = document.getElementById("chest4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
