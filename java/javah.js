0a454794cabbbfdc58f01f179c483810be4e9eeb
kavi678 <kavipatel111.kp@gmail.com>
2018-10-05 03:08:16 +0930

    woman and man BMR added and calorie calculator added

--- java/javah.js
+++ java/javah.js
@@ -55,3 +55,81 @@ function mybpm() {
         x.style.display = "none";
     }
 }
+function mywoman() {
+    var x = document.getElementById("womanhide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myman() {
+    var x = document.getElementById("manhide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function mycal() {
+    var x = document.getElementById("mhide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+(function () {
+	function calculatemanBMR(manWeight, manHeight, manAge) {
+		manWeight = parseFloat(manWeight);
+		manHeight = parseFloat(manHeight);
+		manAge = parseFloat(manAge);
+		return ((manWeight * 10) + (manHeight * 6.25) - (manAge *5) + 5);
+	}
+
+	var manBMR = document.getElementById("manBMR");
+	if (manBMR) {
+		manBMR.onsubmit = function () {
+			this.result.value = calculatemanBMR(this.manWeight.value, this.manHeight.value, this.manAge.value);
+			return false;
+		};
+	}
+}());
+
+
+
+
+(function () {
+	function calculatewomanBMR(womanWeight, womanHeight, womanAge) {
+		womanWeight = parseFloat(womanWeight);
+		womanHeight = parseFloat(womanHeight);
+		womanAge = parseFloat(womanAge);
+		return ((womanWeight * 10) + (womanHeight * 6.25) - (womanAge *5) - 161);
+	}
+
+	var womanBMR = document.getElementById("womanBMR");
+	if (womanBMR) {
+		womanBMR.onsubmit = function () {
+			this.result.value = calculatewomanBMR(this.womanWeight.value, this.womanHeight.value, this.womanAge.value);
+			return false;
+		};
+	}
+}());
+
+
+(function () {
+	function MacrosToCalories(proteins, carbs, fats) {
+		proteins = parseFloat(proteins);
+		carbs = parseFloat(carbs);
+		fats = parseFloat(fats);
+		return ((proteins * 4) + (carbs * 4) + (fats * 9));
+	}
+
+	var calculateMacros = document.getElementById("calculateMacros");
+	if (calculateMacros) {
+		calculateMacros.onsubmit = function () {
+			this.result.value = MacrosToCalories(this.proteins.value, this.carbs.value, this.fats.value);
+			return false;
+		};
+	}
+}());
